88db97b88a1d052a6bd9eb33d2d3a4219a454429
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-11-12 14:59:08 +0400

    Update EndGame styles, add icon
    Co-Authored-By: zouascur <4364771+zouascur@users.noreply.github.com>

--- src/app/components/EndGame/EndGame.styled.tsx
+++ src/app/components/EndGame/EndGame.styled.tsx
@@ -7,25 +7,17 @@ export const EndGameContainer = styled.div`
   justify-content: center;
   background-color: #ccffe0;
   flex-direction: column;
-  width: 400px;
-  height: 200px;
-  border: 3px dotted #14b554;
-  border-radius: 5%;
-  box-shadow: 5px 8px 15px 10px rgba(234, 187, 122, 0.2);
-  background: rgb(255, 222, 173);
-  background: rgb(255, 222, 173);
-  background: linear-gradient(
-    346deg,
-    rgba(255, 222, 173, 1) 0%,
-    rgba(204, 255, 224, 1) 52%,
-    rgba(237, 149, 97, 1) 100%
-  );
+  width: 300px;
+  max-width: 70vw;
+  aspect-ratio: 2 / 1;
+  border-radius: 1%;
+  background-color: #ffdca1;
 `;
 
 export const Heading2 = styled.h2`
   font-weight: 600;
-  margin-bottom: 0.7rem;
-  color: #088539;
-  text-shadow: 2px 2px 2px #14b554;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+  color: #ff6347;
   text-align: center;
 `;
--- src/app/components/EndGame/EndGame.tsx
+++ src/app/components/EndGame/EndGame.tsx
@@ -1,10 +1,12 @@
 import { EndGameContainer, Heading2 } from './EndGame.styled';
-
+import { FaChildReaching, FaPeopleRoof } from 'react-icons/fa6';
 export default function EndGame() {
   return (
     <EndGameContainer>
       <Heading2>Поздравляем, это победа!</Heading2>
       <Heading2>Вы нашли своего малыша.💚</Heading2>
+      {/* <FaChildReaching size={60} /> */}
+      <FaPeopleRoof size={60} color="rgba(255, 99, 71, 0.5)" />
     </EndGameContainer>
   );
 }
